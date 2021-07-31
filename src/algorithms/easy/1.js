da2ce9041c0ae85176f14771887b0accda26586d
ChrisLi <christopherli1018@gmail.com>
2021-07-31 01:53:01 -0400

    add 1

---
+++ src/algorithms/easy/1.js
@@ -0,0 +1,12 @@
+// 1. Two Sum
+// https://leetcode.com/problems/two-sum/
+
+const twoSum = (nums, target) => {
+  const hashT = {}
+  for (let i = 0; i < nums.length; i++) {
+    const toFind = target - nums[i]
+    if (toFind in hashT) return [hashT[toFind], i]
+    hashT[nums[i]] = i
+  }
+  return []
+}
