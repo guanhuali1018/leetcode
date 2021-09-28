642fa78f2f3800a8972772ed02141042764f61b5
ChrisLi <christopherli1018@gmail.com>
2021-09-28 00:14:23 -0400

    add 652

---
+++ src/algorithms/medium/652.js
@@ -0,0 +1,23 @@
+// 652. Find Duplicate Subtrees
+// https://leetcode.com/problems/find-duplicate-subtrees/
+
+var findDuplicateSubtrees = function (root) {
+  const map = new Map(),
+    res = []
+  dfs(root, map, res)
+  return res
+}
+
+function dfs(root, map, res) {
+  if (!root) return '#'
+  const subtree = `${root.val}.${dfs(root.left, map, res)}.${dfs(
+    root.right,
+    map,
+    res
+  )}`
+  map.set(subtree, (map.get(subtree) || 0) + 1)
+  if (map.get(subtree) === 2) {
+    res.push(root)
+  }
+  return subtree
+}
