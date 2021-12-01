c7ce985d1ad482623de05361888f1b08f293de9a
ChrisLi <christopherli1018@gmail.com>
2021-12-01 00:48:23 -0500

    add 138

--- src/algorithms/medium/138.js
+++ src/algorithms/medium/138.js
@@ -1,36 +1,24 @@
 // 138. Copy List with Random Pointer
 // https://leetcode.com/problems/copy-list-with-random-pointer/
 
+// https://leetcode.com/problems/copy-list-with-random-pointer/discuss/689730/JavaScript-Solution
 var copyRandomList = function (head) {
   if (!head) return null
 
-  let arr = []
-  let res = new Node()
-  let t = res
+  const map = new Map([[null, null]])
 
-  while (head) {
-    t.next = dfs(head, arr)
-    t = t.next
-    head = head.next
+  let curr = head
+  while (curr !== null) {
+    map.set(curr, new Node(curr.val))
+    curr = curr.next
   }
 
-  return res.next
-}
+  curr = head
+  while (curr !== null) {
+    map.get(curr).next = map.get(curr.next)
+    map.get(curr).random = map.get(curr.random)
+    curr = curr.next
+  }
 
-function dfs(node, arr) {
-  if (!node) return null
-  if (node.visited) return arr[node.ref]
-
-  let newNode = new Node(node.val)
-  arr.push(newNode)
-
-  // Under normal circumstances in programming, we shouldn't really modify the original data
-  // But JS being JS doesn't have real hash tables, and or unique objectId we can use as keys
-  // for POJO
-
-  node.visited = true
-  node.ref = arr.length - 1
-  newNode.random = dfs(node.random, arr)
-
-  return newNode
+  return map.get(head)
 }
