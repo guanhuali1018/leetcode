157b2bf1202089990c88b147d92d99fd4f67cf12
ChrisLi <christopherli1018@gmail.com>
2021-12-13 07:47:30 -0500

    add 1167

---
+++ src/algorithms/medium/1167.js
@@ -0,0 +1,30 @@
+// 1167. Minimum Cost to Connect Sticks
+// https://leetcode.com/problems/minimum-cost-to-connect-sticks/
+
+const connectSticks = function(sticks) {
+  if (sticks.length === 1) {
+    return 0
+  }
+  
+  sticks.sort((a, b) => a - b)
+  
+  let sum = [], result = 0
+  
+  while (sticks.length || (sum.length > 1)) {
+    let cur = 0
+    for (let i = 0; i < 2; i++) {
+      if (sticks[0] && (sum[0] === undefined || (sticks[0] < sum[0]))) {
+        cur += sticks[0]
+        sticks.shift()
+      } else {
+        cur += sum[0]
+        sum.shift()
+      }
+    }
+    
+    sum.push(cur)
+    result += cur
+  }
+  
+  return result
+}
