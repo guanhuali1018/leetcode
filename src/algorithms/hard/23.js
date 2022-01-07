3a9a6ec0aec0772fd8e0c646afbf53087f56c8b9
ChrisLi <christopherli1018@gmail.com>
2022-01-07 08:42:08 -0500

    add 23

---
+++ src/algorithms/hard/23.js
@@ -0,0 +1,30 @@
+// 23. Merge k Sorted Lists
+// https://leetcode.com/problems/merge-k-sorted-lists/
+
+const mergeKLists = (lists) => {
+  const merge = (l1, l2) => {
+    if (!l1 || !l2) return l1 || l2
+    let node = {}
+    const root = node
+    while (l1 && l2) {
+      if (l1.val <= l2.val) {
+        node.next = l1
+        l1 = l1.next
+      } else {
+        node.next = l2
+        l2 = l2.next
+      }
+      node = node.next
+    }
+    if (l1) node.next = l1
+    if (l2) node.next = l2
+    return root.next
+  }
+
+  let root = lists[0]
+  for (let i = 1; i < lists.length; i++) {
+    root = merge(root, lists[i])
+  }
+
+  return root || null
+}
