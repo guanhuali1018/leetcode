9a6b35d7865cb8b58ae6ca4b4b2fab4019eb233d
ChrisLi <christopherli1018@gmail.com>
2022-01-13 08:37:34 -0500

    add 704

---
+++ src/algorithms/easy/704.js
@@ -0,0 +1,19 @@
+// 704. Binary Search
+// https://leetcode.com/problems/binary-search/
+
+const search = function (nums, target) {
+  let left = 0
+  let right = nums.length - 1
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2)
+
+    if (nums[mid] === target) {
+      return mid
+    } else if (nums[mid] < target) {
+      left = mid + 1
+    } else right = mid - 1
+  }
+
+  return -1
+}
