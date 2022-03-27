819351e8f93ea7bedcae8bd18c89fb805136e5cd
Guanhua Li <christopherli1018@gmail.com>
2022-03-27 01:21:32 -0400

    test

--- src/algorithms/easy/1.js
+++ src/algorithms/easy/1.js
@@ -2,11 +2,12 @@
 // https://leetcode.com/problems/two-sum/
 
 const twoSum = (nums, target) => {
-  const hashT = {}
+  const hashT = {};
   for (let i = 0; i < nums.length; i++) {
-    const toFind = target - nums[i]
-    if (toFind in hashT) return [hashT[toFind], i]
-    hashT[nums[i]] = i
+    const toFind = target - nums[i];
+    if (toFind in hashT) return [hashT[toFind], i];
+    hashT[nums[i]] = i;
   }
-  return []
-}
+  return [];
+};
+// test
